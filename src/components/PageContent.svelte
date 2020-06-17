49ee34802ba7fe7219344051c65885bd8308733e
Connor Zwick <czoop@me.com>
2020-06-16 21:37:08 -0700

    Made changes to content storage

--- src/components/PageContent.svelte
+++ src/components/PageContent.svelte
@@ -1,43 +1,59 @@
 <script>
   import { tick } from "svelte"
 
-  let text = "Hello, start typing your notes..."
+  let content = [
+    { classes: ["normal"], text: "Type your notes here...", el: null },
+    { classes: ["bold"], text: "BOLD!", el: null },
+  ]
+
+  $: console.log(content)
 
   async function handleKeydown(event) {
     let selection = window.getSelection()
-    console.log(selection)
+    let range = selection.getRangeAt(0)
+    let container = range.startContainer
 
-    // Save all the ranges
-    // let range = selection.getRangeAt(0)
+    console.log(container)
 
-    // Change the text
-    text = text.toUpperCase()
+    // Find the matching object
+    let part = content.filter((p) => range.intersectsNode(p.el))
+    console.log(part)
+
+    let start = range.startOffset
+    let end = range.endOffset
+    // console.log(container, start, end)
 
     // Restore the ranges
-    await tick()
-    let text_node = this.childNodes[0]
-    selection.removeAllRanges()
-
-    let new_range = document.createRange()
-    new_range.setStart(text_node, 0)
-    new_range.setEnd(text_node, 5)
-
-    selection.addRange(new_range)
-    console.log(this)
+    // await tick()
+    // let text_node = this.childNodes[0]
+    // selection.removeAllRanges()
+    // let new_range = document.createRange()
+    // new_range.setStart(text_node, 0)
+    // new_range.setEnd(text_node, 5)
+    // selection.addRange(new_range)
   }
-
-  // let handleKeydown = (event) => {
-  //   console.log(this)
-  // }
 </script>
 
 <style>
   div {
     font-size: 1.5rem;
   }
+
+  .normal {
+    font-style: normal;
+  }
+
+  .bold {
+    font-weight: bold;
+  }
 </style>
 
-<div
-  contenteditable="true"
-  bind:textContent={text}
-  on:keydown|preventDefault={handleKeydown} />
+<div on:keydown|preventDefault={handleKeydown}>
+  {#each content as { classes, text, el }}
+    <span
+      contenteditable="true"
+      bind:textContent={text}
+      bind:this={el}
+      class={classes} />
+  {/each}
+</div>
