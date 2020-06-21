1617c8e06cfc39ce54ca4ccb69751027ac3cbf49
Connor Zwick <czoop@me.com>
2020-06-21 15:34:14 -0700

    Fixed page ratio

--- src/components/ZoomController.js
+++ src/components/ZoomController.js
@@ -1,12 +1,12 @@
 import { writable, derived } from "svelte/store"
 
-const width_ratio = 8
+const width_ratio = 8.5
 const height_ratio = 11
 
 export const width_px = writable(850)
 export const height_px = derived(
-  width_px,
-  (w) => w * (height_ratio / width_ratio)
+    width_px,
+    w => w * (height_ratio / width_ratio)
 )
 
-export const page_padding_px = derived(width_px, (w) => w / width_ratio)
+export const page_padding_px = derived(width_px, w => w / width_ratio)
