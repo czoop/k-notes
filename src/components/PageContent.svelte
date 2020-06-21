970320b3b4218a11480dbd11a2ee5efeb750f1c9
Connor Zwick <czoop@me.com>
2020-06-21 15:34:30 -0700

    Basic strategy

--- src/components/PageContent.svelte
+++ src/components/PageContent.svelte
@@ -1,64 +1,103 @@
 <script>
-  import { tick } from "svelte"
+    import { tick } from "svelte"
 
-  let content = [
-    { classes: ["normal"], text: "Type your notes here...", el: null },
-    { classes: ["bold"], text: "BOLD!", el: null },
-  ]
+    let content = []
+    let parentEl = null
 
-  $: console.log(content)
+    $: console.log(content)
 
-  async function handleKeydown(event) {
-    await tick()
-    for (let i = 0; i < content.length; i++) {
-      let part = content[i]
-      content[i] = { ...part, text: part.el.textContent }
+    async function handleInput(event) {
+        if (parentEl !== null) {
+            for (let i = 0; i < parentEl.childNodes.length; i++) {
+                content[i] = parentEl.childNodes[i]
+            }
+        }
     }
-  }
 
-  // async function handleKeydown(event) {
-  //   let selection = window.getSelection()
-  //   let range = selection.getRangeAt(0)
-  //   let startContainer = range.startContainer
+    // async function handleInput(event) {
+    //     if (parentEl !== null) {
+    //         let children = parentEl.childNodes
+    //         console.log("children", children)
 
-  //   let selectionContents = range.cloneContents()
-  //   let extractContents = range.extractContents()
-  //   console.log(
-  //     extractContents,
-  //     selectionContents,
-  //     extractContents === selectionContents
-  //   )
-  //   range.deleteContents()
+    //         for (let i = 0; i < children.length; i++) {
+    //             let subChildren = children[i].childNodes
+    //             console.log("subchildren", subChildren)
+    //             for (let j = 0; j < subChildren.length; j++) {
+    //                 content[i].subChildren[j] = subChildren[j].textContent
+    //             }
+    //         }
+    //         console.log(content)
+    //     }
+    // }
 
-  //   console.log(extractContents, selectionContents)
+    // async function handleKeydown(event) {
+    //     if (event.key !== "b") return
+    //     console.log(event.key)
 
-  //   // Insert the clone as a new text block and then select it
-  //   let startBlockIndex = content.findIndex((p) => p.el === startContainer)
-  //   content.splice(startBlockIndex, 0, {
-  //     classes: ["bold"],
-  //     text: selectionContents.textContent,
-  //     el: null,
-  //   })
-  //   content = content // For Svelte
-  // }
+    //     let selection = window.getSelection()
+    //     let range = selection.getRangeAt(0)
+    //     let startContainer = range.startContainer
+    //     let startOffset = range.startOffset
+    //     let endContainer = range.endContainer
+    //     let endOffset = range.endOffset
+
+    //     let selectionContents = range.extractContents()
+    //     range.deleteContents()
+
+    //     let startIndex = content.findIndex(p => p.el === startContainer)
+    //     let endIndex = content.findIndex(p => p.el === endContainer)
+
+    //     // Add class to sections inbetween start and end
+    //     for (let i = startIndex + 1; i < endIndex; i++) {
+    //         let classes = content[i].classes
+    //         classes = new Set(classes)
+    //         classes.add("bold")
+    //         content[i].classes = classes
+    //     }
+    // }
+
+    // async function handleKeydown(event) {
+    //   let selection = window.getSelection()
+    //   let range = selection.getRangeAt(0)
+    //   let startContainer = range.startContainer
+
+    //   let selectionContents = range.cloneContents()
+    //   let extractContents = range.extractContents()
+    //   console.log(
+    //     extractContents,
+    //     selectionContents,
+    //     extractContents === selectionContents
+    //   )
+    //   range.deleteContents()
+
+    //   console.log(extractContents, selectionContents)
+
+    //   // Insert the clone as a new text block and then select it
+    //   let startBlockIndex = content.findIndex((p) => p.el === startContainer)
+    //   content.splice(startBlockIndex, 0, {
+    //     classes: ["bold"],
+    //     text: selectionContents.textContent,
+    //     el: null,
+    //   })
+    //   content = content // For Svelte
+    // }
 </script>
 
 <style>
-  div {
-    font-size: 1.5rem;
-  }
+    div {
+        font-size: 1.5rem;
+        outline: none;
+    }
 
-  .normal {
-    font-style: normal;
-  }
+    .parent {
+        height: 100%;
+    }
 
-  .bold {
-    font-weight: bold;
-  }
+    .bold {
+        font-weight: bold;
+    }
 </style>
 
-<div contenteditable="true" on:input={handleKeydown}>
-  {#each content as { classes, text, el }}
-    <span bind:this={el} class={classes}>{text}</span>
-  {/each}
+<div bind:this={parentEl} contenteditable="true" on:input={handleInput}>
+    <br />
 </div>
