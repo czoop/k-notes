9bddd10ad9f704af148dc8c12e86aa501da283fc
Connor Zwick <czoop@me.com>
2020-06-27 20:25:37 -0700

    Attempted using index to only update single content obj

--- src/components/PageContent.svelte
+++ src/components/PageContent.svelte
@@ -3,44 +3,12 @@
 
     let content = [{ subChildren: [{ text: "Hello", classes: ["bold"] }] }]
 
-    /** @type HTMLDivElement */
+    /** @type {HTMLDivElement} */
     let parentEl = null
 
     $: console.log("content", content)
 
-    // async function handleInput(event) {
-    //     if (parentEl !== null) {
-    //         for (let i = 0; i < parentEl.childNodes.length; i++) {
-    //             content[i] = parentEl.childNodes[i]
-    //         }
-    //     }
-    // }
-
-    // async function handleInput(event) {
-    //     await tick()
-
-    //     if (parentEl !== null) {
-    //         let children = parentEl.childNodes
-    //         console.log("children", children)
-
-    //         for (let i = 0; i < children.length; i++) {
-    //             let subChildren = children[i].childNodes
-    //             console.log("subchildren", subChildren)
-
-    //             if (!content[i]) {
-    //                 // content[i] = { subChildren: [] }
-    //             }
-
-    //             for (let j = 0; j < subChildren.length; j++) {
-    //                 // content[i].subChildren[j] = {
-    //                 //     text: subChildren[j].textContent,
-    //                 //     classes: subChildren[j].classList,
-    //                 // }
-    //             }
-    //         }
-    //     }
-    // }
-
+    /** @param {Event} event */
     async function handleInput(event) {
         await tick()
 
@@ -48,13 +16,41 @@
             return
         }
 
-        console.log("children", parentEl.children)
         let children = parentEl.children
+        console.log("children", children)
 
-        for (let i = 0; i < children.length; i++) {}
+        let selection = window.getSelection()
+        let node = selection.anchorNode
+
+        if (node instanceof Text) {
+            node = node.parentElement.parentElement
+        }
+
+        let node_index = Array.prototype.indexOf.call(children, node)
+        console.log("node", node_index, node)
+
+        // Let's just update content[node_index]
+        content[node_index] = {
+            subChildren: [
+                {
+                    text: parentEl.children[node_index].textContent,
+                    classes: parentEl.children[node_index].classList,
+                },
+            ],
+        }
+
+        // for (let i = 0; i < children.length; i++) {
+        //     let child = children[i]
+        //     content[i] = {
+        //         subChildren: [
+        //             { text: child.textContent, classes: child.classList },
+        //         ],
+        //     }
+        // }
     }
 
     async function handleKeydown(event) {
+        return
         if (event.key !== "b") return
         event.preventDefault
 
@@ -72,69 +68,16 @@
 
         selectionContents.classList.add("bold")
     }
-
-    // async function handleKeydown(event) {
-    //     if (event.key !== "b") return
-    //     console.log(event.key)
-
-    //     let selection = window.getSelection()
-    //     let range = selection.getRangeAt(0)
-    //     let startContainer = range.startContainer
-    //     let startOffset = range.startOffset
-    //     let endContainer = range.endContainer
-    //     let endOffset = range.endOffset
-
-    //     let selectionContents = range.extractContents()
-    //     range.deleteContents()
-
-    //     let startIndex = content.findIndex(p => p.el === startContainer)
-    //     let endIndex = content.findIndex(p => p.el === endContainer)
-
-    //     // Add class to sections inbetween start and end
-    //     for (let i = startIndex + 1; i < endIndex; i++) {
-    //         let classes = content[i].classes
-    //         classes = new Set(classes)
-    //         classes.add("bold")
-    //         content[i].classes = classes
-    //     }
-    // }
-
-    // async function handleKeydown(event) {
-    //   let selection = window.getSelection()
-    //   let range = selection.getRangeAt(0)
-    //   let startContainer = range.startContainer
-
-    //   let selectionContents = range.cloneContents()
-    //   let extractContents = range.extractContents()
-    //   console.log(
-    //     extractContents,
-    //     selectionContents,
-    //     extractContents === selectionContents
-    //   )
-    //   range.deleteContents()
-
-    //   console.log(extractContents, selectionContents)
-
-    //   // Insert the clone as a new text block and then select it
-    //   let startBlockIndex = content.findIndex((p) => p.el === startContainer)
-    //   content.splice(startBlockIndex, 0, {
-    //     classes: ["bold"],
-    //     text: selectionContents.textContent,
-    //     el: null,
-    //   })
-    //   content = content // For Svelte
-    // }
 </script>
 
 <style>
     div {
-        font-size: 1.5rem;
         outline: none;
         overflow-wrap: break-word;
     }
 
     .parent {
-        height: 100%;
+        font-size: 1.5rem;
     }
 
     .bold {
@@ -144,12 +87,15 @@
 
 <div
     bind:this={parentEl}
+    class="parent"
     contenteditable="true"
-    on:input={handleInput}
-    on:keydown={handleKeydown}>
-    {#each content as { subChildren }, line_number (line_number)}
-        {#each subChildren as part, index (index)}
-            <div class={part.classes}>{part.text}</div>
-        {/each}
+    on:keydown={handleKeydown}
+    on:input={handleInput}>
+    {#each content as { subChildren }, j (j)}
+        <div>
+            {#each subChildren as part, i (i)}
+                <span class={part.classes}>{part.text}</span>
+            {/each}
+        </div>
     {/each}
 </div>
