f8444e2fd2d08254480accf6614ff0936dc39907
Connor Zwick <czoop@me.com>
2020-06-28 12:52:29 -0700

    Created new rendering method

--- src/components/PageContent.svelte
+++ src/components/PageContent.svelte
@@ -1,119 +1,54 @@
 <script>
     import { tick, onDestroy, onMount } from "svelte"
-    import findIndex from "lodash/findIndex"
-    import lodash_range from "lodash/range"
+    import { h1, h2, h3, h4, h5, h6, span } from "./TypeMappings"
 
-    // *** VARIABLES ***
     let content = [
-        {
-            parts: [
-                { text: "Hello", classes: [] },
-                { text: " Hello2", classes: [] },
-            ],
-        },
-        { parts: [{ text: "World", classes: [] }] },
+        { text: "Heading 1", type: "h1" },
+        { text: "Heading 2", type: "h2" },
+        { text: "Heading 3", type: "h3" },
+        { text: "Heading 4", type: "h4" },
+        { text: "Heading 5", type: "h5" },
+        { text: "Heading 6", type: "h6" },
+        { text: "Normal text ", type: "span" },
+        { text: "Bold text ", type: "span", classes: ["bold"] },
+        { text: "Italic text", type: "span", classes: ["italics"] },
     ]
-
-    /** @type {HTMLDivElement} */
-    let parentEl = null
-
-    // *** LOGS ***
-    $: console.log("content", content)
-
-    // *** FUNCTIONS ***
-
-    /** Handles populating the information when the page is first created */
-    onMount(() => {
-        // Insert any saved text
-        for (let i = 0; i < content.length; i++) {
-            let div = document.createElement("div")
-            for (let j = 0; j < content[i].parts.length; j++) {
-                let span = document.createElement("span")
-                span.textContent = content[i].parts[j].text
-                span.classList = content[i].parts[j].classes
-                div.appendChild(span)
-            }
-            parentEl.appendChild(div)
-        }
-    })
-
-    /** @param {Event} event */
-    async function handleInput(event) {
-        let children = parentEl.children
-        let new_content = []
-
-        // Rare case - When there's only one line of text
-        if (
-            Array.prototype.findIndex.call(
-                children,
-                c => c instanceof HTMLDivElement
-            ) === -1
-        ) {
-            children = parentEl.childNodes
-        }
-
-        // Go through the child divs and populate content with the info
-        for (let i = 0; i < children.length; i++) {
-            let div = children[i]
-            for (let j = 0; j < div.children.length; j++) {
-                let span = children[i].children[j]
-                new_content[i] = {
-                    parts: [
-                        { text: span.textContent, classes: span.classList },
-                    ],
-                }
-            }
-        }
-
-        content = new_content
-    }
-
-    /** @param {KeyboardEvent} event */
-    function handleKeydown(event) {
-        if (event.key === "b") {
-            let selection = window.getSelection()
-
-            if (selection.isCollapsed) {
-                return
-            }
-
-            event.preventDefault()
-
-            let range = selection.getRangeAt(0)
-            let children = parentEl.children
-            console.log(children, range.startContainer, range.endContainer)
-            let startDiv = range.startContainer.parentElement
-            let endDiv = range.endContainer.parentElement
-            let startDivIndex = findIndex(children, c => c === startDiv)
-            let endDivIndex = findIndex(children, c => c === endDiv)
-            console.log(startDivIndex, endDivIndex)
-
-            let indexesInBetween = lodash_range(startDivIndex + 1, endDivIndex)
-
-            let extract = range.extractContents()
-            console.log(extract)
-        }
-    }
 </script>
 
 <style>
     div {
-        outline: none;
+        /* outline: none; */
         overflow-wrap: break-word;
     }
 
     .parent {
-        font-size: 1.5rem;
     }
 
     .bold {
         font-weight: bold;
     }
+
+    .italics {
+        font-style: italic;
+    }
 </style>
 
-<div
-    bind:this={parentEl}
-    class="parent"
-    contenteditable="true"
-    on:input={handleInput}
-    on:keydown={handleKeydown} />
+<div class="parent">
+    {#each content as { text, type, classes }}
+        {#if type === span}
+            <span class={classes}>{text}</span>
+        {:else if type === h1}
+            <h1>{text}</h1>
+        {:else if type === h2}
+            <h2>{text}</h2>
+        {:else if type === h3}
+            <h3>{text}</h3>
+        {:else if type === h4}
+            <h4>{text}</h4>
+        {:else if type === h5}
+            <h5>{text}</h5>
+        {:else if type === h6}
+            <h6>{text}</h6>
+        {/if}
+    {/each}
+</div>
---
+++ src/components/TypeMappings.js
@@ -0,0 +1,10 @@
+const h1 = "h1"
+const h2 = "h2"
+const h3 = "h3"
+const h4 = "h4"
+const h5 = "h5"
+const h6 = "h6"
+
+const span = "span"
+
+export { h1, h2, h3, h4, h5, h6, span }
